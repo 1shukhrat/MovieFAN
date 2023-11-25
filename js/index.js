42840abd00f325f3d4e9d58ce067525e00d4a7fb
Shukhrat <saynurdinov1998@gmail.com>
2023-11-25 22:35:54 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,133 @@
+document.addEventListener("DOMContentLoaded", function() {
+   fetchMovies();
+ });
+ 
+ let currentPage = 0;
+ let isLastPage = false;
+ let selectedGenre = '';
+ let selectedCountry = '';
+ 
+ function fetchMovies() {
+  let url = `http://localhost:8080/api/v1/movies?page=${currentPage}`;
+  if (selectedGenre) {
+    url += `&genre=${selectedGenre}`;
+  }
+  if (selectedCountry) {
+    url += `&country=${selectedCountry}`;
+  }
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      if (data.length === 0 ) {
+        isLastPage = true;
+      }
+      displayMovies(data);
+    })
+    .catch(error => console.error('Ошибка:', error));
+ }
+ 
+ function displayMovies(movies) {
+   const container = document.getElementById('moviesContainer');
+   if (currentPage == 0) {
+    container.innerHTML = ' ';
+   }
+   
+   movies.forEach(movie => {
+     const card = document.createElement('div');
+     card.className = 'movie-card';
+
+     card.setAttribute('data-movie-id', movie.id);
+ 
+     const image = document.createElement('img');
+     image.src = movie.posterUrl; 
+     image.className = 'movie-image';
+ 
+     const title = document.createElement('h2');
+     title.className = 'movie-title';
+     title.textContent = movie.title; 
+ 
+     card.appendChild(image);
+     card.appendChild(title);
+
+     card.addEventListener('click', function()  {
+      window.location.href = `movie.html?id=${this.getAttribute('data-movie-id')}`;
+     })
+    
+ 
+     container.appendChild(card);
+   });
+ }
+
+ document.getElementById('genreSelect').addEventListener('change', function() {
+  selectedGenre = this.value;
+  currentPage = 0;
+  isLastPage = false;
+  fetchMovies();
+});
+
+document.getElementById('countrySelect').addEventListener('change', function() {
+  selectedCountry = this.value;
+  currentPage = 0;
+  isLastPage = false;
+  fetchMovies();
+});
+ 
+ let timeout;
+ window.addEventListener('scroll', () => {
+   clearTimeout(timeout);
+   timeout = setTimeout(() => {
+     if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 && !isLastPage) {
+       document.getElementById('loadingIndicator').style.display = 'block';
+       currentPage++;
+       fetchMovies();
+     }
+     else {
+      document.getElementById('loadingIndicator').style.display = 'none';
+     }
+   }, 100);
+ });
+
+ let genresLoaded = false;
+ let countriesLoaded = false;
+ 
+ document.getElementById('genreSelect').addEventListener('focus', function() {
+   if (!genresLoaded) {
+     fetchGenres();
+   }
+ });
+ 
+ document.getElementById('countrySelect').addEventListener('focus', function() {
+   if (!countriesLoaded) {
+     fetchCountries();
+   }
+ });
+ 
+ function fetchGenres() {
+   fetch('http://localhost:8080/api/v1/genres') // Замените URL_API_ЖАНРОВ на ваш URL
+     .then(response => response.json())
+     .then(genres => {
+       populateDropdown(document.getElementById('genreSelect'), genres);
+       genresLoaded = true;
+     })
+     .catch(error => console.error('Ошибка при получении жанров:', error));
+ }
+ 
+ function fetchCountries() {
+   fetch('http://localhost:8080/api/v1/countries') // Замените URL_API_СТРАН на ваш URL
+     .then(response => response.json())
+     .then(countries => {
+       populateDropdown(document.getElementById('countrySelect'), countries);
+       countriesLoaded = true;
+     })
+     .catch(error => console.error('Ошибка при получении стран:', error));
+ }
+ 
+ function populateDropdown(selectElement, items) {
+   items.forEach(item => {
+     const option = document.createElement('option');
+     option.value = item.name;
+     option.textContent = item.name;
+     selectElement.appendChild(option);
+   });
+ }
+ 
---
+++ js/movie.js
@@ -0,0 +1,144 @@
+document.addEventListener("DOMContentLoaded", function() {
+   const movieId = new URLSearchParams(window.location.search).get('id');
+   fetchMovieDetails(movieId);
+   insertKinoboxPlayer(movieId);
+   fetchMovieStaff(movieId);
+
+   const crewList = document.getElementById('movieCrew');
+    const leftScrollButton = document.querySelector('.left-scroll');
+    const rightScrollButton = document.querySelector('.right-scroll');
+
+    leftScrollButton.addEventListener('click', () => {
+        crewList.scrollLeft -= 100; // Можно настроить значение
+    });
+
+    rightScrollButton.addEventListener('click', () => {
+        crewList.scrollLeft += 100; // Можно настроить значение
+    });
+ });
+ 
+ function fetchMovieDetails(movieId) {
+   fetch(`http://localhost:8080/api/v1/movies/${movieId}`) 
+     .then(response => response.json())
+     .then(movie => {
+       displayMovieDetails(movie);
+       document.getElementById('ratingValue').textContent = movie.userRating.toFixed(1);
+     })
+     .catch(error => console.error('Ошибка:', error));
+ }
+ 
+ function displayMovieDetails(movie) {
+   document.getElementById('posterImage').src = movie.posterUrl;
+   document.getElementById('movieTitle').textContent = movie.title;
+ 
+   // Заполнение деталей фильма
+   const detailsList = document.getElementById('movieDetails');
+   detailsList.innerHTML = '';
+ 
+   const outlineItem = document.createElement('li');
+   outlineItem.textContent = `Описание: ${movie.outline}`;
+   detailsList.appendChild(outlineItem);
+
+   const yearItem = document.createElement('li');
+   yearItem.textContent = `Год: ${movie.yearOfRelease}`;
+   detailsList.appendChild(yearItem);
+ 
+   const countriesItem = document.createElement('li');
+   countriesItem.textContent = `Страны: ${movie.countries.map(country => country.name).join(', ')}`;
+   detailsList.appendChild(countriesItem);
+ 
+   const genresItem = document.createElement('li');
+   genresItem.textContent = `Жанры: ${movie.genres.map(genre => genre.name).join(', ')}`;
+   detailsList.appendChild(genresItem);
+ }
+
+ function insertKinoboxPlayer(movieId) {
+   const kinoboxContainer = document.getElementById('kinobox-container');
+ 
+   const kinoboxPlayer = document.createElement('div');
+   kinoboxPlayer.className = 'kinobox_player';
+   kinoboxPlayer.style.width = '1000px';
+   kinoboxContainer.appendChild(kinoboxPlayer);
+ 
+   const kinoboxScript = document.createElement('script');
+   kinoboxScript.src = 'https://kinobox.tv/kinobox.min.js';
+   document.body.appendChild(kinoboxScript);
+ 
+   kinoboxScript.onload = () => {
+     new Kinobox('.kinobox_player', { 
+      search: { 
+         kinopoisk: movieId 
+      },
+      hide: ['alloha'],
+   }).init();
+   };
+ }
+
+ document.getElementById('submitComment').addEventListener('click', () => {
+  const commentText = document.getElementById('commentText').value;
+  if (commentText) {
+    const comment = document.createElement('div');
+    comment.className = 'comment';
+    comment.textContent = commentText;
+    document.querySelector('.comments-list').appendChild(comment);
+
+    // Очистить текстовое поле
+    document.getElementById('commentText').value = '';
+
+    // Здесь можно добавить отправку комментария на сервер
+  }
+});
+
+function fetchMovieStaff(movieId) {
+  fetch(`http://localhost:8080/api/v1/movies/${movieId}/staff`) 
+    .then(response => response.json())
+    .then(staff => {
+      displayMovieStaff(staff);
+    })
+    .catch(error => console.error('Ошибка при получении данных о съемочной группе:', error));
+}
+
+function displayMovieStaff(staff) {
+  const crewContainer = document.getElementById('movieCrew');
+    crewContainer.innerHTML = '';
+
+    staff.directors.forEach(director => {
+        const directorItem = createCrewMemberListItem(director.fullName, null, "Режиссёр");
+        crewContainer.appendChild(directorItem);
+    });
+
+    staff.actors.forEach(actor => {
+        const actorItem = createCrewMemberListItem(actor.fullName, actor.photoUrl, "Актёр");
+        crewContainer.appendChild(actorItem);
+    });
+}
+
+function createCrewMemberListItem(name, photoUrl, role) {
+  const listItem = document.createElement('li');
+  listItem.classList.add('crew-member');
+
+  const img = document.createElement('img');
+  img.src = photoUrl ? photoUrl : '../images/no-poster.gif'; // Замените на путь к вашему дефолтному изображению
+  img.alt = name;
+  img.classList.add('crew-photo');
+  listItem.appendChild(img);
+
+  const nameElement = document.createElement('div');
+  nameElement.textContent = name;
+  nameElement.classList.add('crew-name');
+  listItem.appendChild(nameElement);
+
+  const roleElement = document.createElement('div');
+  roleElement.textContent = role;
+  roleElement.classList.add('crew-role');
+  listItem.appendChild(roleElement);
+
+  return listItem;
+}
+
+
+
+
+
+
+ 
